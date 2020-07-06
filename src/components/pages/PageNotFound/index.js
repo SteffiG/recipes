a16f160e3968ac4471dc73d1a65462cc8d3c2094
SteffiG <gutierrezsteffi@gmail.com>
2020-07-06 16:37:07 -0500

    page not found

--- src/components/pages/PageNotFound/index.js
+++ src/components/pages/PageNotFound/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './PageNotFound.css';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => (
     <div className="page">
-         <p className="page">Uy, llegaste a un mundo desconocido. Mejor regresa al inicio.</p>
+        <p className="page-description">Uy, llegaste a un mundo desconocido. Mejor regresa al <Link to="/" className="page-link">inicio.</Link></p>
+        <img className="image" src="https://www.pfchangs.com/images/default-source/default-album/website-404errorpage-v23f59853857be6e10a67fff0000af721d.jpg?sfvrsn=90e7eb35_0" alt="404" />
     </div>   
 );
 
